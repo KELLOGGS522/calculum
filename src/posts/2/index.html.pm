f5742f802b7f58b6e310acf76c29b248b1e1028c
Mattéo Delabre <git.matteo@delab.re>
2022-09-20 13:25:34 -0400

    Annonce de la première rencontre

---
+++ src/posts/2/index.html.pm
@@ -0,0 +1,13 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{8d755039-e340-49e5-baef-efc566ac6fa1}
+◊define-meta[title]{Rencontre hebdomadaire du club #1}
+◊define-meta[date]{2022-09-22T17:30:00-04:00}
+◊define-meta[summary]{Cette première rencontre sera l’occasion de poursuivre la discussion sur les activités du club. La première série de problèmes y sera également présentée.}
+
+◊a[#:href (local-url "")]{← Retour à l’accueil}
+
+◊h1{◊post-title[]}
+
+Cette première rencontre sera l’occasion de poursuivre la discussion sur les activités du club.
+La première série de problèmes y sera également présentée.
