0a957181fc231c715eab04bc03dbacaf88ef7088
Mattéo Delabre <git.matteo@delab.re>
2022-09-20 13:25:13 -0400

    Résumé de la présentation du club

--- src/posts/1/index.html.pm
+++ src/posts/1/index.html.pm
@@ -3,13 +3,19 @@
 ◊define-meta[uuid]{013493af-2eeb-4bb8-8528-598d2df46d07}
 ◊define-meta[title]{Présentation du Calculum}
 ◊define-meta[date]{2022-09-15T17:30:00-04:00}
-◊define-meta[summary]{Présentation de l’univers de la programmation compétitive et des activités futures du club d’algorithmique compétitive. Au local AA-3195. Durée 30 min–1 h.}
+◊define-meta[summary]{Présentation de l’univers de la programmation compétitive et des activités futures du club d’algorithmique compétitive. Au local AA-3195. Durée 30 min.}
 
 ◊a[#:href (local-url "")]{← Retour à l’accueil}
 
 ◊h1{◊post-title[]}
 
-Si la programmation compétitive ou les casse-têtes informatiques en général vous intéressent, joignez-vous à nous pour cette présentation du Calculum.
-Ce moment sera l’occasion de présenter l’univers de la programmation compétitive et de discuter des activités futures du club.
-La présentation aura lieu le ◊strong{jeudi 15 septembre à 17 h 30 au local AA-3195} et durera entre 30 minutes et 1 heure.
-Si vous ne pouvez pas assister, un résumé sera mis en ligne à cet endroit dans les jours qui suivent.
+Cette présentation a introduit l’univers de la programmation compétitive, les principaux concours universitaires, et a été l’occasion d’une discussion avec les personnes intéressées concernant les activités futures du club.
+Les activités proposées sont des réunions hebdomadaires autour de problèmes de programmation compétitive, la constitution d’équipes pour des compétitions comme l’ICPC, les CS Games et la Battlecode, ainsi que l’organisation de concours internes au DIRO.
+Certaines personnes dans l’assistance ont manifesté leur intérêt pour des workshops présentant plus en détail les différents algorithmes utiles en programmation compétitive.
+Un sondage a été lancé pour déterminer les horaires des réunions hebdomadaires. 
+
+◊ul{
+    ◊li{◊a[#:href "https://rallly.co/p/Cydl6zchn0ys"]{Sondage horaire →}}
+    ◊li{◊a[#:href (local-url "files/2022-09-16-Presentation-Calculum.pdf")]{Transparents de la présentation →}}
+    ◊li{◊a[#:href "https://www.youtube.com/watch?v=RGFDMNLWMJA"]{Enregistrement de la présentation →}}
+}
