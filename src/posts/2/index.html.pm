0216f6e50e51f7f2ea20ed0432f96c8439d31422
Mattéo Delabre <git.matteo@delab.re>
2022-09-22 14:12:56 -0400

    Première série de problèmes

--- src/posts/2/index.html.pm
+++ src/posts/2/index.html.pm
@@ -9,5 +9,15 @@
 
 ◊h1{◊post-title[]}
 
-Cette première rencontre sera l’occasion de poursuivre la discussion sur les activités du club.
-La première série de problèmes y sera également présentée.
+Pour commencer, nous poursuivrons la discussion sur les activités du club. Venez proposer vos idées !
+La première série de problèmes sera ensuite introduite.
+Cette série a pour thématique la ◊em{géométrie} :
+
+◊ul[#:class "long-list"]{
+    ◊li{(Pour tous) ◊a[#:href "https://open.kattis.com/problems/pieceofcake2"]{◊em{“Piece of Cake!”}} — Un gâteau en forme de parallélépipède a été divisé en quatre parts rectilignes. Calculez le volume de la plus grande des parts.}
+    ◊li{(Intermédiaire) ◊a[#:href "https://open.kattis.com/problems/triangleornaments"]{◊em{“Triangle Ornaments”}} — On suspend une guirlande de triangles, chaque triangle étant attaché par l’un de ses sommets. Étant données la mesure des côtés de chaque triangle, déterminez la largeur qu’ils occupent.}
+    ◊li{(Avancé) ◊a[#:href "https://open.kattis.com/problems/segmentintersection"]{◊em{“Line Segment Intersection”}} — Calculez l’intersection de deux segments du plan dont on vous donne les coordonnées cartésiennes des extrémités. Attention aux cas particuliers!}
+}
+
+Nous commencerons à travailler sur ces problèmes.
+Si vous parvenez à résoudre l’un des problèmes, venez nous présenter votre solution la semaine prochaine!
