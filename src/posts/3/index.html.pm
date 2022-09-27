ffdf8498c7a97558f8f1d9dd268335e6e8c3ffd6
Mattéo Delabre <git.matteo@delab.re>
2022-09-27 15:03:55 -0400

    Annonce deuxième rencontre

---
+++ src/posts/3/index.html.pm
@@ -0,0 +1,20 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{f124e088-fba7-4510-9b4e-79fd36ccd375}
+◊define-meta[title]{Rencontre hebdomadaire du club #2}
+◊define-meta[date]{2022-09-29T17:30:00-04:00}
+◊define-meta[summary]{Dans cette deuxième rencontre, nous discuterons des solutions de la série de problèmes géométriques. Nous travaillerons ensuite sur une nouvelle série de problèmes liés aux chaînes de caractères.}
+
+◊a[#:href (local-url "")]{← Retour à l’accueil}
+
+◊h1{◊post-title[]}
+
+Dans cette deuxième rencontre, nous commencerons par discuter des solutions de la série de problèmes sur la géométrie. N’hésitez pas à venir présenter votre solution! La plupart des problèmes peuvent être résolus de plusieurs façons différentes, et il est toujours intéressant de comparer ces solutions.
+
+Nous travaillerons ensuite sur une nouvelle série de problèmes liés aux chaînes de caractères. Voici quelques ressources utiles pour lire et pratiquer sur ce sujet:
+
+◊ul[#:class "long-list"]{
+    ◊li{le chapitre 2 du livre ◊em{“Programmation efficace”} de C. Dürr et J.-J. Vie traitent d’algorithmes pour les chaînes de caractères et proposent des implémentations en Python ◊a[#:href "https://umontreal.on.worldcat.org/oclc/945921847"]{(une copie du livre est à la bibliothèque)};}
+    ◊li{les sections 14.1 et 14.2 du livre ◊em{“Guide to Competitive Programming”} de A. Laaksonen couvrent également les algorithmes essentiels pour ce sujet ◊a[#:href "https://umontreal.on.worldcat.org/oclc/1154565111"]{(une version numérique du livre est disponible depuis le site web de la bibliothèque)};}
+    ◊li{la section ◊em{“String Algorithms”} ◊a[#:href "https://cses.fi/problemset/"]{du site CSES} regroupe des problèmes classiques de chaînes de caractères auxquels vous pouvez vous essayer (il suffit de ◊a[#:href "https://cses.fi/register/"]{créer un compte} pour pouvoir soumettre).}
+}
