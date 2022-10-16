b27b5297fba6b8b3c9be4c8c46734dc74e2619de
Mattéo Delabre <git.matteo@delab.re>
2022-10-16 00:32:58 -0400

    Rencontre #5: Annonce

---
+++ src/posts/6/index.html.pm
@@ -0,0 +1,8 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{2b044bdc-4692-4fe8-bb92-99d3cffd4a3e}
+◊define-meta[title]{Rencontre hebdomadaire du club #5}
+◊define-meta[date]{2022-10-20T17:30:00-04:00}
+◊define-meta[summary]{Nous travaillerons sur les problèmes de graphes annoncés la semaine dernière.}
+
+Nous travaillerons sur les ◊in-link["posts/5/#problems"]{problèmes de graphes annoncés la semaine dernière}.
