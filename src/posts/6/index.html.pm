347cc5dd96265f703ebcfaa6402f15fc40b300d8
Mattéo Delabre <git.matteo@delab.re>
2022-10-23 12:04:46 -0400

    Résumé rencontre #6 et annonce rencontre #7

--- src/posts/6/index.html.pm
+++ src/posts/6/index.html.pm
@@ -3,6 +3,6 @@
 ◊define-meta[uuid]{2b044bdc-4692-4fe8-bb92-99d3cffd4a3e}
 ◊define-meta[title]{Rencontre hebdomadaire du club #5}
 ◊define-meta[date]{2022-10-20T17:30:00-04:00}
-◊define-meta[summary]{Nous travaillerons sur les problèmes de graphes annoncés la semaine dernière.}
+◊define-meta[summary]{Nous avons travaillé sur les problèmes de graphes annoncés la semaine précédente.}
 
-Nous travaillerons sur les ◊in-link["posts/5/#problems"]{problèmes de graphes annoncés la semaine dernière}.
+Nous avons travaillé sur les ◊in-link["posts/5/#problems"]{problèmes de graphes annoncés la semaine précédente}.
---
+++ src/posts/7/index.html.pm
@@ -0,0 +1,9 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{950504ce-43e7-4281-b745-a3d8225ca591}
+◊define-meta[title]{Rencontre hebdomadaire du club #6}
+◊define-meta[date]{2022-11-03T17:30:00-04:00}
+◊define-meta[summary]{Pas de rencontre pendant la semaine de relâche. Au retour, nous discuterons des solutions des problèmes de graphes et débuterons une nouvelle série de problèmes.}
+
+Il n’y aura pas de rencontre pendant la semaine de relâche.
+Au retour, nous discuterons des solutions des problèmes de graphes et débuterons une nouvelle série de problèmes.
