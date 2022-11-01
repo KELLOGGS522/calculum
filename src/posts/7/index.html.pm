9f2e71892ae593ad7b7827ca54eb24665f90d9d2
Mattéo Delabre <git.matteo@delab.re>
2022-11-01 09:37:20 -0400

    Annonce de la 6ᵉ rencontre

--- src/posts/7/index.html.pm
+++ src/posts/7/index.html.pm
@@ -3,7 +3,17 @@
 ◊define-meta[uuid]{950504ce-43e7-4281-b745-a3d8225ca591}
 ◊define-meta[title]{Rencontre hebdomadaire du club #6}
 ◊define-meta[date]{2022-11-03T17:30:00-04:00}
-◊define-meta[summary]{Pas de rencontre pendant la semaine de relâche. Au retour, nous discuterons des solutions des problèmes de graphes et débuterons une nouvelle série de problèmes.}
+◊define-meta[summary]{Nous reprendrons nos rencontres hebdomadaires en commençant par discuter des solutions aux problèmes de graphes. Nous débuterons ensuite une nouvelle série de problèmes sur la programmation dynamique.}
 
-Il n’y aura pas de rencontre pendant la semaine de relâche.
-Au retour, nous discuterons des solutions des problèmes de graphes et débuterons une nouvelle série de problèmes.
+Nous reprendrons nos rencontres hebdomadaires en commençant par discuter des solutions aux problèmes de graphes.
+Nous débuterons ensuite une nouvelle série de problèmes sur la programmation dynamique.
+
+◊link-h2["resources"]{Ressources}
+
+Si vous ne connaissez pas la programmation dynamique, ou si vous avez besoin de vous rafraîchir la mémoire, voici quelques ressources utiles à lire:
+
+◊ul[#:class "long-list"]{
+    ◊li{la section 1.6.5 du ◊out-link["https://umontreal.on.worldcat.org/oclc/945921847"]{livre “Programmation efficace”} donne une petite introduction au concept de programmation dynamique, ainsi que des astuces utiles pour l’implémenter en Python;}
+    ◊li{le chapitre 6 du ◊out-link["https://umontreal.on.worldcat.org/oclc/1154565111"]{livre “Guide to Competitive Programming”} de A. Laaksonen entre dans plus de détails et détaille des exemples classiques de programmation dynamique, implémentés en C++;}
+    ◊li{la section ◊out-link["https://cp-algorithms.com/dynamic_programming/divide-and-conquer-dp.html"]{“Dynamic Programming”} du ◊out-link["https://cp-algorithms.com/"]{site web cp-algorithms.com} traite de sujets plus avancés en programmation dynamique.}
+}
