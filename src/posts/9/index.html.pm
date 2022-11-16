201255188f4fd523d9736b562f733ba204c95553
Mattéo Delabre <git.matteo@delab.re>
2022-11-16 10:18:20 -0500

    Corrige ID post en double

--- src/posts/9/index.html.pm
+++ src/posts/9/index.html.pm
@@ -1,6 +1,6 @@
 #lang pollen
 ◊; vim: set spelllang=fr:
-◊define-meta[uuid]{13242137-80d3-4eff-b68c-ec0136adf6e2}
+◊define-meta[uuid]{1197fd0f-5f87-40ac-89f1-e14f7db79955}
 ◊define-meta[title]{Rencontre #8: Programmation dynamique (revue)}
 ◊define-meta[date]{2022-11-17T17:30:00-05:00}
 ◊define-meta[summary]{Nous passerons en revue ensemble les problèmes de programmation dynamique et leurs solutions, et nous discuterons du concours d’entraînement à venir la semaine prochaine.}
