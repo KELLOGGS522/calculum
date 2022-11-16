3dd4c2df8741dfeb5edf47cb5d25910b665c3b7d
Mattéo Delabre <git.matteo@delab.re>
2022-11-15 22:48:07 -0500

    Annonce de la 8e rencontre

--- src/posts/8/index.html.pm
+++ src/posts/8/index.html.pm
@@ -2,7 +2,7 @@
 ◊; vim: set spelllang=fr:
 ◊define-meta[uuid]{13242137-80d3-4eff-b68c-ec0136adf6e2}
 ◊define-meta[title]{Rencontre #7: Programmation dynamique (suite)}
-◊define-meta[date]{2022-11-10T17:30:00-04:00}
-◊define-meta[summary]{Nous travaillerons sur les problèmes de programmation dynamique annoncés la semaine précédente.}
+◊define-meta[date]{2022-11-10T17:30:00-05:00}
+◊define-meta[summary]{Nous avons travaillé sur les problèmes de programmation dynamique annoncés la semaine précédente.}
 
-Nous travaillerons sur ◊in-link["posts/7/#problems"]{les problèmes de programmation dynamique} annoncés la semaine précédente.
+Nous avons travaillé sur ◊in-link["posts/7/#problems"]{les problèmes de programmation dynamique} annoncés la semaine précédente.
---
+++ src/posts/9/index.html.pm
@@ -0,0 +1,8 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{13242137-80d3-4eff-b68c-ec0136adf6e2}
+◊define-meta[title]{Rencontre #8: Programmation dynamique (revue)}
+◊define-meta[date]{2022-11-17T17:30:00-05:00}
+◊define-meta[summary]{Nous passerons en revue ensemble les problèmes de programmation dynamique et leurs solutions, et nous discuterons du concours d’entraînement à venir la semaine prochaine.}
+
+Nous passerons en revue ensemble les problèmes de programmation dynamique et leurs solutions, et nous discuterons du concours d’entraînement à venir la semaine prochaine.
