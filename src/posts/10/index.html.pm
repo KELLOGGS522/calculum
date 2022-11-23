5e44d4b06562c6c7b6312aae6199eff1d0e19a16
Mattéo Delabre <git.matteo@delab.re>
2022-11-23 01:16:24 -0500

    Annonce du concours d’entraînement

---
+++ src/posts/10/index.html.pm
@@ -0,0 +1,19 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{d35a83f1-2262-4bb8-b7db-2a2716fa67d5}
+◊define-meta[title]{Concours d’entraînement}
+◊define-meta[date]{2022-11-24T17:30:00-05:00}
+◊define-meta[summary]{Cette rencontre prendra exceptionnellement la forme d’un concours d’entraînement. Pendant deux heures, vous devrez résoudre le plus de problèmes possible parmi 5 problèmes qui seront dévoilés le jour même.}
+
+Cette semaine, notre rencontre prendra exceptionnellement la forme d’un concours d’entraînement.
+L’objectif est de se mettre dans des conditions similaires à celles d’un concours de programmation.
+Pendant deux heures, vous devrez ◊em{résoudre le plus de problèmes possible} parmi 5 problèmes qui seront dévoilés le jour même.
+Ces problèmes porteront sur les thématiques vues pendant les semaines précédentes (géométrie, chaînes de caractères, graphes, programmation dynamique).
+
+Le concours aura lieu sur Kattis.
+Vous pourrez soumettre des tentatives entre 17:35 et 19:35.
+Le classement se fera en ordre décroissant selon le nombre de problèmes résolus, puis, en cas d’égalité, en ordre croissant selon la somme du temps pris à résoudre les problèmes, plus une pénalité de 20 minutes pour chaque erreur.
+
+◊out-link["https://open.kattis.com/contests/n25qzb"]{Accéder au concours →}
+
+Si vous n’avez pas déjà reçu une invitation pour rejoindre le concours par courriel, envoyez votre nom d’utilisateur Kattis à Mattéo.
