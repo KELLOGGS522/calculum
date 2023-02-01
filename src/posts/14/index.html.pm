18adc03d4217629c494c4638e55e7cb96ff1984e
Mattéo Delabre <git.matteo@delab.re>
2023-02-01 16:12:56 -0500

    Résumé de la 12ᵉ rencontre

---
+++ src/posts/14/index.html.pm
@@ -0,0 +1,9 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{db5f07fc-872c-43d7-b054-d0c01be1d8a6}
+◊define-meta[title]{Rencontre #12: Mathématiques (suite)}
+◊define-meta[date]{2023-01-25T17:00:00-05:00}
+◊define-meta[summary]{Nous avons discuté des solutions aux problèmes de la semaine passée. Les solutions apparaîtront sur cette page prochainement.}
+
+Nous avons discuté des solutions aux ◊in-link["posts/13"]{problèmes de la semaine passée}.
+Les solutions apparaîtront sur cette page prochainement.
