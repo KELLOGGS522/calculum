9482784e714c975c90123212c0f7e16d917cd023
Mattéo Delabre <git.matteo@delab.re>
2023-02-15 09:35:09 -0500

    Problèmes de la rencontre 15

---
+++ src/posts/17/index.html.pm
@@ -0,0 +1,23 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{cbd99ed9-297e-4fe3-b47d-2257a431a60f}
+◊define-meta[title]{Rencontre #15: Graphes}
+◊define-meta[date]{2023-02-15T17:00:00-05:00}
+◊define-meta[summary]{Nous étudierons cette semaine une série de problèmes sur les graphes.}
+
+Nous étudierons cette semaine une série de problèmes sur les graphes.
+Voyez aussi notre ◊in-link["posts/5/#problems"]{précédente série de problèmes de graphes}, étudiée à l’automne.
+
+◊link-h2["problems"]{Problèmes}
+
+Les problèmes suivants sont triés par ordre croissant de difficulté.
+
+◊ol[#:class "long-list"]{
+    ◊li{◊out-link["https://open.kattis.com/problems/islandhopping"]{“Island Hopping”} — Comment relier un ensemble d’îles par des ponts en minimisant la longueur totale des ponts construits?}
+    ◊li{◊out-link["https://open.kattis.com/problems/mazemovement"]{“Maze Movement”} — Considérez des salles reliées par des corridors ayant une capacité maximale de personnes par minute. Quelle est le nombre maximum de personnes par minute pouvant se rendre de la première salle à la dernière?}
+    ◊li{◊out-link["https://open.kattis.com/problems/shortestpath3"]{“Single source shortest path, negative weights”} — ◊em{Tout est dans le titre!}}
+    ◊li{◊out-link["https://open.kattis.com/problems/bookclub"]{“Book Club”} — Chaque personne arrive avec une pile de livres à donner et une liste de livres qu’elle accepte de recevoir. Existe-t-il une façon pour que les personnes échangent leurs livres de sorte que chacune en reçoive un nouveau?}
+    ◊li{◊out-link["https://open.kattis.com/problems/catenyms"]{“Catenyms”} — Deux mots sont des ◊em{caténymes} si le premier termine par la même lettre que la première lettre du second. À partir d’une liste de mots, donnez une chaîne de caténymes qui les visite tous exactement une fois.}
+}
+
+◊em{Bonus: ◊out-link["https://open.kattis.com/problems/golfbot"]{“Golf Bot”} (problème intéressant mais sans rapport avec les graphes).}
