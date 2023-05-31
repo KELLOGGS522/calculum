76ed199e244d738ee7bb707e0fda87cbea0200a8
Mattéo Delabre <git.matteo@delab.re>
2023-05-31 13:54:05 -0400

    Problèmes de la rencontre #20

---
+++ src/posts/23/index.html.pm
@@ -0,0 +1,22 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{d248a42e-e2ce-45c7-b541-4ed8bb688785}
+◊define-meta[title]{Rencontre #20: Radar}
+◊define-meta[date]{2023-05-17T17:30:00-04:00}
+◊define-meta[summary]{Nous débutons cette semaine les rencontres d’été du club, avec des problèmes choisis en référence au club RADAR nouvellement formé pendant l’hiver.}
+
+Cette semaine marque le début des rencontres d’été du club.
+Les problèmes de cette semaine font appel à diverses techniques algorithmiques.
+Ils ont été choisis pour leur thématique en référence au ◊out-link["https://radar-iro.github.io"]{club RADAR} nouvellement formé pendant l’hiver.
+
+◊link-h2["problems"]{Problèmes}
+
+Les problèmes suivants sont triés par ordre croissant de difficulté.
+
+◊ol[#:class "long-list"]{
+    ◊li{◊out-link["https://open.kattis.com/problems/remorse"]{“ReMorse”} — Trouvez une façon d’attribuer une séquence de points et tirets à chaque lettre pour obtenir le codage le plus court possible pour un mot donné.}
+    ◊li{◊out-link["https://open.kattis.com/problems/countingstars"]{“Counting Stars”} — Combien d’étoiles sont visibles dans une image du ciel?}
+    ◊li{◊out-link["https://open.kattis.com/problems/communicationssatellite"]{“Communication Satellite”} — Placez des supports à des endroits appropriés pour soutenir la structure d’un satellite en minimisant la quantité de matériau utilisé.}
+    ◊li{◊out-link["https://open.kattis.com/problems/radiotransmission"]{“Radio Transmission”} — Identifiez un message à partir d’un extrait de communication dans lequel le message est répété plusieurs fois.}
+    ◊li{◊out-link["https://open.kattis.com/problems/radar"]{“Radar”} — Utilisez la phase de trois signaux reçus d’une tour radio pour obtenir une borne inférieure sur la distance qui vous sépare.}
+}
