af0e63778e3b22cae687f0cf4ef67c5396bee7d3
Mattéo Delabre <git.matteo@delab.re>
2023-06-21 19:21:13 -0400

    Résumé de la rencontre #23

---
+++ src/posts/26/index.html.pm
@@ -0,0 +1,37 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{56f8e821-5889-4f9a-b737-6de5947349b7}
+◊define-meta[title]{Rencontre #23: Fin de la première saison}
+◊define-meta[date]{2023-06-21T17:30:00-04:00}
+◊define-meta[summary]{Aujourd’hui a eu lieu la dernière rencontre du club pour la saison 2022–2023.}
+
+Alors que s’achève la première saison du Calculum, il est temps pour une courte rétrospective de nos activités.
+
+De la ◊in-link["posts/1"]{première rencontre du club} en septembre dernier à aujourd’hui,
+les membres du Calculum ont eu à se pencher sur pas moins de ◊em{78 problèmes de programmation compétitive}.
+Chacun de ces problèmes leur a permis d’aiguiser leurs compétences en programmation et en algorithmique,
+dans une atmosphère de réflexion partagée.
+
+Outre la résolution de problèmes, les membres se sont aussi illustrés dans
+le ◊in-link["posts/19"]{calcul du nombre π},
+le ◊em{code golfing},
+la ◊in-link["posts/4/#problems"]{résolution (?) de problèmes ouverts},
+et la participation à des concours de programmation.
+
+En particulier, au mois de février, quatre équipes du club, 
+les 
+◊em{Useless IT Squad},
+◊em{The Waddledees},
+◊em{Greedy Boys},
+et ◊em{Natural Golfers},
+ont fièrement représenté l’université en
+◊out-link["https://wiki.aediroum.ca/wiki/ICPC#2022–2023"]{en participant à la division régionale du concours ICPC.}
+Une partie de nos membres a également
+◊out-link["https://wiki.aediroum.ca/wiki/CS_Games#2023"]{participé à l’édition 2023 des CS Games}.
+
+Grâce aux efforts de ses membres, l’université occupe désormais la troisième position du ◊out-link["https://open.kattis.com/countries/CAN/QC"]{classement des universités du Québec sur Kattis}, et est en passe de se hisser à la deuxième place!
+Nous poursuivrons cet effort au cours de la prochaine saison.
+
+Merci à ◊out-link["https://www-ens.iro.umontreal.ca/~miloszro"]{Robin}, qui a donné l’impulsion aux clubs du DIRO et nous a aidé tout au long de l’année.
+Merci à toutes les personnes qui ont participé au club pendant cette saison.
+J’espère vous revoir nombreuses et nombreux, aux côtés de la nouvelle cohorte, à l’automne prochain!
