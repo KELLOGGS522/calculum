95e6d32da4dd95441e23ca1b1736f3dacdaec3c7
Mattéo Delabre <git.matteo@delab.re>
2023-06-21 19:20:42 -0400

    Résumé de la rencontre #22

---
+++ src/posts/25/index.html.pm
@@ -0,0 +1,21 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{04e0f895-46a9-48e1-9be0-c5281b6a2885}
+◊define-meta[title]{Rencontre #22: Problèmes du NWERC 2022}
+◊define-meta[date]{2023-06-14T17:30:00-04:00}
+◊define-meta[summary]{Les problèmes de cette semaine sont tirés du concours régional NWERC 2022.}
+
+Les problèmes de cette semaine sont tirés du concours régional ◊out-link["https://2022.nwerc.eu/"]{NWERC 2022} de l’ICPC, qui s’est tenu en novembre dernier.
+La thématique des problèmes alterne principalement entre géométrie et combinatoire.
+
+◊link-h2["problems"]{Problèmes}
+
+Les problèmes suivants sont triés par ordre croissant de difficulté.
+
+◊ol[#:class "long-list"]{
+    ◊li{◊out-link["https://open.kattis.com/problems/bottleflip"]{“Bottle Flip”} — À quel niveau doit-on remplir une bouteille d’eau afin que son centre de masse soit le plus bas possible?}
+    ◊li{◊out-link["https://open.kattis.com/problems/circularcaramelcookie"]{“Circular Caramel Cookie”} — Calculez la dimension minimale d’un disque pour qu’il puisse contenir ◊math{s} carrés d’aire unitaire.}
+    ◊li{◊out-link["https://open.kattis.com/problems/highqualitytree"]{“High-quality Tree”} — Combien de fois au moins faut-il retirer une feuille d’un arbre binaire pour le rendre équilibré?}
+    ◊li{◊out-link["https://open.kattis.com/problems/eta"]{“ETA”} — Construisez un graphe dont la distance moyenne à un sommet donné égale une quantité voulue.}
+    ◊li{◊out-link["https://open.kattis.com/problems/lastguess"]{“Last Guess”} — Résolvez le jeu Wordle.}
+}
