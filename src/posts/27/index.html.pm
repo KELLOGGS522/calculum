ea25052c43c61d414e3e4f1a2bc5ee36db510aba
Mattéo Delabre <git.matteo@delab.re>
2023-10-09 17:16:30 -0400

    Rencontres 24 et 25

---
+++ src/posts/27/index.html.pm
@@ -0,0 +1,12 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{11dc8277-f8a1-465e-a7ca-075d3bc9524c}
+◊define-meta[title]{Rencontre #24: Stratégies de résolution}
+◊define-meta[date]{2023-10-02T18:30:00-04:00}
+◊define-meta[summary]{Un aperçu des différentes stratégies applicables aux problèmes de programmation compétitive: recherche exhaustive, programmation dynamique, diviser pour régner, vorace.}
+
+Après avoir lu le sujet d’un problème de programmation compétitive, la première étape pour construire une solution est de décider comment le programme va rechercher sa solution.
+Certaines techniques sont applicables seulement dans certaines circonstances tandis d’autres sont universelles mais plus lentes à l’exécution.
+Voici un aperçu des princpales stratégies de recherche de solution et de comment choisir la bonne.
+
+◊in-link["files/2023-10-02-Strategies-Resolution.pdf"]{Transparents sur les stratégies de résolution →}
---
+++ src/posts/28/index.html.pm
@@ -0,0 +1,20 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{3c663f99-3295-46a2-a975-eda7a997a749}
+◊define-meta[title]{Rencontre #25: Combinatoire et graphes}
+◊define-meta[date]{2023-10-09T18:30:00-04:00}
+◊define-meta[summary]{Nous étudions cette semaine des problèmes en lien avec de la combinatoire et/ou des graphes.}
+
+Nous étudions cette semaine des problèmes en lien avec de la combinatoire et/ou des graphes.
+
+◊link-h2["problems"]{Problèmes}
+
+Les problèmes suivants sont triés par ordre croissant de difficulté.
+
+◊ol[#:class "long-list"]{
+    ◊li{◊out-link["https://open.kattis.com/problems/flexible"]{“Flexible Spaces”} — Calculez toutes les valeurs atteignables en faisant la différence de deux valeurs dans une liste.}
+    ◊li{◊out-link["https://open.kattis.com/problems/flipfive"]{“Flip Five”} — Trouvez la longueur de la meilleure séquence de coups permettant de transformer une grille binaire en une autre, selon certaines règles.}
+    ◊li{◊out-link["https://open.kattis.com/problems/pebblesolitaire"]{“Pebble Solitaire”} — Dans cette variante du ◊out-link["https://fr.wikipedia.org/wiki/Solitaire_(casse-t%C3%AAte)"]{solitaire} à une dimension, vous devez calculer une séquence de coups permettant d’éliminer le plus de pions possible.}
+    ◊li{◊out-link["https://open.kattis.com/problems/erraticants"]{“Erratic Ants”} — Optimisez la longueur du chemin emprunté par une fourmi, en évitant les détours inutiles.}
+    ◊li{◊out-link["https://open.kattis.com/problems/pagelayout"]{“Page Layout”} — Trouvez la plus grande aire qui peut être couverte en positionnant des rectangles sans chevauchement sur un plan.}
+}
