fd4ef9e21e41a744964ac7e7b34c484cb76f23ed
KELLOGGS522 <laguimauve522@gmail.com>
2024-10-02 17:17:01 -0400

    add post 39

---
+++ src/posts/39/index.html.pm
@@ -0,0 +1,19 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{6182244d-5982-4e01-94fc-0c5c230204b4}
+◊define-meta[title]{Rencontre #36: Tri}
+◊define-meta[date]{2024-10-02T18:00:00-05:00}
+◊define-meta[summary]{Tri (Sorting)}
+◊define-meta[author]{Guillaume Tardif et Samuel Maltais}
+
+◊link-h2["problems"]{Problèmes}
+
+Le nombre à côté de chaque problème indique son niveau de difficulté selon Kattis.
+
+◊ol[#:class "long-list"]{
+    ◊problem["mjehuric" "Mjehuric" "1.7"]{Simuler un tri étape par étape.}
+    ◊problem["height" "Height" "1.9"]{Donner le nombre d’étape lors du tri de l’input.}
+    ◊problem["dyslectionary" "Dyslectionary" "2.8"]{Trier des groupes de mot dans l’ordre dyslectionary.}
+    ◊problem["mali" "Mali" "5.2"]{Trouver la plus petite somme maximum à chaque ronde.}
+    ◊problem["booking" "Booking" "5.3"]{Trouver le nombre minimum de chambre nécéssaire.}
+}
