3813fc3fdcb90c632afb18b2d163b435884933c8
KELLOGGS522 <laguimauve522@gmail.com>
2024-11-27 16:18:53 -0500

    post41

---
+++ src/posts/41/index.html.pm
@@ -0,0 +1,19 @@
+#lang pollen
+◊; vim: set spelllang=fr:
+◊define-meta[uuid]{e220b33a-149f-40b9-a6f3-bf89eeaba065}
+◊define-meta[title]{Rencontre #38: Pratique problèmes ICPC Régionaux antérieurs}
+◊define-meta[date]{2024-11-20T18:00:00-05:00}
+◊define-meta[summary]{Pratique problèmes ICPC Régionaux antérieurs)}
+◊define-meta[author]{Guillaume Tardif et Samuel Maltais}
+
+◊link-h2["problems"]{Problèmes}
+
+Le nombre à côté de chaque problème indique son niveau de difficulté selon Kattis.
+
+◊ol[#:class "long-list"]{
+    ◊problem["ultimatebinarywatch" "Ultimate Binary Watch" "2.0"]{}
+    ◊problem["espresso" "Espresso!" "2.1"]{}
+    ◊problem["codeguessing" "Code Guessing" "3.0"]{}
+    ◊problem["subprime" "Subprime" "3.8"]{}
+    ◊problem["landequality" "Land Equality" "5.1"]{}
+}
